970ab0e3886b07d542ed7af504c4f57bad46c422
hoka cristian <hokacristian@gmail.com>
2023-03-19 15:15:50 +0700

    Green House Monitoring

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // api url
-const api_url = "https://api.thingspeak.com/channels/<ChannelID>/feeds.json?api_key=<ReadAPIKeys>&results=1";
+const api_url = "https://api.thingspeak.com/channels/2057016/feeds.json?api_key=MFMQ4VJBG2C34T3F&results=1";
 // masukan url sesuai dengan format <ChannelID>, <ReadAPIKeys> yang ada di Thingspeak, tanpa menggunakan < >
 
 setInterval(() => {
@@ -8,7 +8,9 @@ setInterval(() => {
     .then((res) => {
       var field = JSON.stringify(res.feeds[0]);
       var obj = JSON.parse(field);
-      document.getElementById("field-1").innerHTML = obj.field1;
-      document.getElementById("field-2").innerHTML = obj.field2;
+      document.getElementById("C").innerHTML = obj.field1;
+      document.getElementById("K").innerHTML = obj.field2;
+      document.getElementById("A").innerHTML = obj.field3;
+      document.getElementById("L").innerHTML = obj.field4;
     });
 }, 1000);
